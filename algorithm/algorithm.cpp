11ce362472c4eea1d8b5876654e5c6a19715cc69
mongobaba <chaowei.meng@gmail.com>
2021-03-02 17:00:09 +0800

    添加gtest和gmock

--- algorithm/algorithm.cpp
+++ algorithm/algorithm.cpp
@@ -1,9 +1,13 @@
 ﻿// algorithm.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-#include <iostream>
+#include <gtest/gtest.h>
 
-int main()
+#pragma comment(lib, "gtestd.lib")
+#pragma comment(lib, "gmockd.lib")
+
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
